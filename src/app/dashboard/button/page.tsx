788220f4397832295598cdd41099261f88f13135
Rafa Belalcazar <rafaelb@unicauca.edu.co>
2025-06-24 22:19:29 -0500

    feat(dashboard/button): add new button variants with icons
    Add icon buttons, outline button with icon and text, and loading state button to enhance UI functionality and user experience.

--- src/app/dashboard/button/page.tsx
+++ src/app/dashboard/button/page.tsx
@@ -1,4 +1,5 @@
 import { Button } from "@/components/ui/button";
+import { ChevronRightIcon, GitBranchIcon, Loader2Icon } from "lucide-react";
 
 
 export default function ButtonPage() {
@@ -12,7 +13,16 @@ export default function ButtonPage() {
             <Button variant={"link"}>link</Button>
             <Button variant={"secondary"}>secondary</Button>
             <Button variant={"default"}>default</Button>
-
+            <Button variant="secondary" size="icon" className="size-8">
+                <ChevronRightIcon />
+            </Button>
+            <Button variant="outline" size="sm">
+                <GitBranchIcon /> New Branch
+            </Button>
+            <Button size="sm" disabled>
+                <Loader2Icon className="animate-spin" />
+                Please wait
+            </Button>
         </div>
     );
 }
